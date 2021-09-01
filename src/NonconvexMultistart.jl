b4dfb65c4d648e16b4055e6d9c3ce67ecae09838
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-01 22:33:50 +1000

    move code from Nonconvex

--- src/NonconvexMultistart.jl
+++ src/NonconvexMultistart.jl
@@ -1,5 +1,22 @@
 module NonconvexMultistart
 
-# Write your package code here.
+export  HyperoptAlg,
+        HyperoptOptions,
+        RandomSampler,
+        Hyperband,
+        LHSampler,
+        CLHSampler,
+        GPSampler,
+        DeflatedAlg,
+        DeflatedOptions
+
+import Hyperopt
+using Reexport, Parameters, Setfield, ChainRulesCore, Sobol
+@reexport using NonconvexCore
+using NonconvexCore: reset!, isfeasible, @params, VecModel, AbstractOptimizer, AbstractResult
+import NonconvexCore: optimize!, Workspace
+
+include("hyperopt.jl")
+include("deflation.jl")
 
 end
